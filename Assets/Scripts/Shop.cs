7bd07002ccd2190578e5da98cefd4b6f656ff9ed
oham01 <omar.hamze01@estudiant.upf.edu>
2025-05-28 12:11:18 +0200

    Update fix

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -18,27 +18,42 @@ public class Shop : MonoBehaviour
 
     public void SelectTurret(ShopButtonTrigger.TurretType type,TowerSpawner spawner)
     {
+        TowerBlueprint selectedBlueprint = null;
         switch (type)
         {
 
-
             case ShopButtonTrigger.TurretType.Turret1:
-                spawner.SelectTowerToBuild(tower1Blueprint);
-                Debug.Log("Turret 1 selected");
-                
+                selectedBlueprint = tower1Blueprint;
+                //spawner.SelectTowerToBuild(tower1Blueprint);
+               // Debug.Log("Turret 1 selected");
                 break;
             case ShopButtonTrigger.TurretType.Turret2:
-                Debug.Log("Turret 2 selected");
-                spawner.SelectTowerToBuild(tower2Blueprint);
+                selectedBlueprint = tower2Blueprint;
+               // Debug.Log("Turret 2 selected");
+               // spawner.SelectTowerToBuild(tower2Blueprint);
                 break;
             case ShopButtonTrigger.TurretType.Turret3:
-                Debug.Log("Turret 3 selected");
-                spawner.SelectTowerToBuild(tower3Blueprint);
+                selectedBlueprint = tower3Blueprint;
+               // Debug.Log("Turret 3 selected");
+                //spawner.SelectTowerToBuild(tower3Blueprint);
                 break;
             case ShopButtonTrigger.TurretType.Unselect:
                 Debug.Log("Unselected");
                 spawner.DeselectTower();
                 break;
         }
+
+        if (GameStateManager.Instance.diamondCount >= selectedBlueprint.cost)
+        {
+            spawner.SelectTowerToBuild(selectedBlueprint);
+            Debug.Log($"{type} selected");
+        }
+        else
+        {
+            Debug.Log("Not enough diamonds to select this turret.");
+
+        }
     }
+
+
 }
